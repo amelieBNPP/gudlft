0a614d3033b97689f7bcaecd6b6aad933c559647
amelieBNPP <amelie.noury@bnpparibas.com>
2022-02-25 17:46:51 +0100

    :zap: performance(locustfile): update locustfile

--- gudlft/__init__.py
+++ gudlft/__init__.py
@@ -29,14 +29,15 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
-    
+
     from . import auth
     app.register_blueprint(auth.bp)
-    
+
     from . import api
     app.register_blueprint(api.bp)
 
     return app
+
 
 def login_required(f):
     @wraps(f)
